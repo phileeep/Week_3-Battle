0e31c92203381fc9912ba07627f180b4c8cf52ce
Phil Smithies <philsmithies@gmail.com>
2021-04-18 19:19:33 +0200

    removed sessions

--- app.rb
+++ app.rb
@@ -8,8 +8,6 @@ class Battle < Sinatra::Base
     register Sinatra::Reloader
   end
 
-  # enable :sessions
-
   get '/' do 
     erb :index
   end
@@ -28,12 +26,8 @@ class Battle < Sinatra::Base
 
   get '/attack' do 
     @game = $game
-    @game.attack(@game.player2)
     erb :attack
   end
 
   run! if app_file == $0
 end
-
-# $p1name = Player.new(params[:p1_name])
-# $p2name = Player.new(params[:p2_name])
